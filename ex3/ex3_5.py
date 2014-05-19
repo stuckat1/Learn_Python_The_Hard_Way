a171289c73614fe98e0e1b18a878f66b7ef0b9ab
keweima <keweima@yahoo.com>
2014-05-19 10:19:29 -0400

    Ex2 and ex3

---
+++ ex3/ex3_5.py
@@ -0,0 +1,41 @@
+# Print out message
+print "I will now count my chickens:"
+
+# Do some arithmetic
+print "Hens", 25 + 30.0 / 6.0
+
+# Do some arithmetic
+print "Roosters", 100 - 25 * 3.0 % 4
+
+# Print out message
+print "Now I will count the eggs:"
+
+# Do some arithmetic
+print 3 + 2 + 1 - 5 + 4.0 % 2.0 - 1.0 / 4.0 + 6
+
+# Print out message
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Do some arithmetic
+print 3 + 2 < 5 - 7
+
+# Test inequality that returns a boolean
+print "What is 3 + 2?", 3 + 2
+
+# Test inequality that returns a boolean
+print "What is 5 - 7?", 5 - 7
+
+# Print out message
+print "Oh, that's why it's False."
+
+# Print out message
+print "How about some more."
+
+# Test inequality that returns a boolean
+print "Is it greater?", 5 > -2
+
+# Test inequality that returns a boolean
+print "Is it greater or equal?", 5 >= -2
+
+# Test inequality that returns a boolean
+print "Is it less or equal?", 5 <= -2
