cc4a30f803d0d9533365b51a13160a3c893bbc30
keweima <keweima@yahoo.com>
2014-05-19 11:46:14 -0400

    Ex 8, 9 and 10

---
+++ ex10/ex10c.py
@@ -0,0 +1,10 @@
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print "My %s cat requires %s" % ("tabby", fat_cat)
+
---
+++ ex10/ex10d.py
@@ -0,0 +1,8 @@
+tabby_cat = "I'm a \"tabby\" cat"
+persian_cat = 'I\'m a "persian" cat'
+
+print "%r" % tabby_cat
+print "%s" % tabby_cat
+
+print "%r" % persian_cat
+print "%s" % persian_cat
