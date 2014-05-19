af5cb8032cc6a1974aa89233c533ecc4325bf0c4
keweima <keweima@yahoo.com>
2014-05-19 11:03:48 -0400

    Ex 6

---
+++ ex6/ex6_2.py
@@ -0,0 +1,12 @@
+# ONE!
+x = "There are %d types of people." % 10
+
+# TWO!
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+
+# THREE!
+# joke_evaluation = "Isn't that joke so funny?! %r"
+
+# FOUR!
+print w + e
