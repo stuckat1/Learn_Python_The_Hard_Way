c213894e7f01a8c56fc1875b5f8aa34a875e69bc
stuckat1 <keweima@yahoo.com>
2014-05-20 16:32:34 -0400

    Ex 21

---
+++ ex21/ex21_4.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    print "ADDING %d + %d" % (a, b)
+    return a + b
+
+def subtract(a, b):
+    print "SUBTRACTING %d - %d" % (a, b)
+    return a - b
+
+def multiply(a, b):
+    print "MULTIPLYING %d * %d" % (a, b)
+    return a * b
+
+def divide(a, b):
+    print "DIVIDING %d / %d" % (a, b)
+    return a / b
+
+temp = 26
+print "Convert %d Celsius to Fahrenheit = " % temp
+
+print add(32, multiply( temp, divide( 9.0, 5.0)))
