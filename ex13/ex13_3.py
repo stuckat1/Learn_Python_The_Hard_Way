042f869850120f992e7eac79955b197079b0a32a
stuckat1 <keweima@yahoo.com>
2014-05-19 23:14:16 -0400

    Ex 13

---
+++ ex13/ex13_3.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first = argv
+#print "What is your last name? "
+second = raw_input("What is your last name? ")
+
+print "The script is called:", script
+print "Your first name is:", first
+print "Your last name is:", second
