5af4a52bef83cca4850c82687f4dba3693634a84
stuckat1 <keweima@yahoo.com>
2014-05-20 17:46:03 -0400

    Ex 29

---
+++ ex30/ex30_3.py
@@ -0,0 +1,12 @@
+people = 40
+cars = 15
+buses = 30
+
+
+if (cars > people) and (buses > cars) :
+    print "We should take the cars."
+elif cars < people:
+    print "We should not take the cars."
+else:
+    print "We can't decide."
+
