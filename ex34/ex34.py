be78a380dcc842ea24d8adeb30720fa7a51ecb7a
stuckat1 <keweima@yahoo.com>
2014-05-21 10:48:28 -0400

    Ex34

---
+++ ex34/ex34.py
@@ -0,0 +1,27 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "Animals! = ", animals
+
+print "The animal at 1."
+print animals[1]
+
+print "The third (3rd) animal."
+print animals[2]
+
+print "The first (1st) animal."
+print animals[0]
+
+print "The animal at 3."
+print animals[3]
+
+print "The fifth (5th) animal."
+print animals[4]
+
+print "The animal at 2."
+print animals[2]
+
+print "The sixth (6th) animal."
+print animals[5]
+
+print "The animal at 4."
+print animals[4]
