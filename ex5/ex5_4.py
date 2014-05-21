f500de36759200715d2ec099d1bda1437ae169e7
stuckat1 <keweima@yahoo.com>
2014-05-20 22:40:01 -0400

    Rename directories by appending 0.

--- ex5/ex5_4.py
+++
@@ -1,9 +0,0 @@
-kilos_per_lb = 0.453592
-cm_per_inch = 0.393701
-
-print "1 lb = %f kilos" % (1.0 * kilos_per_lb)
-print "5 lb = %f kilos" % (5.0 * kilos_per_lb)
-
-
-print "1 \" = %f cm" % (1.0 * cm_per_inch)
-print "5 \" = %f cm" % (5.0 * cm_per_inch)
--- ex6/ex6_4.py
+++
@@ -1,8 +0,0 @@
-w = "This is the left side of..."
-e = "a string with a right side."
-
-# Print two strings by concatenation
-y = w + e
-
-print y
-print w + e
